143f4d7114c22ecf6279f334163b863ae257ee6f
Dana Mazen <163353421+Dana-Mazen@users.noreply.github.com>
2024-12-27 11:26:21 +0200

    updated (new comment)

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 let lastScrollY = window.scrollY; //for vertical position 
 const navbar = document.getElementById('navbar');
-
+//new comment
 window.addEventListener('scroll', () => {
   if (window.scrollY > lastScrollY) {
     //when scroll down hides nav
